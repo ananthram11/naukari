f9b38c4db56ffd9c50e8373be8a45ed7c0063d0b
ananthram11 <60092918+ananthram11@users.noreply.github.com>
2020-03-26 12:52:16 +0530

    Add files via upload

--- NaukariProject/reports/cucumber-html-report/report.js
+++ NaukariProject/reports/cucumber-html-report/report.js
@@ -45,21 +45,21 @@ formatter.match({
   "location": "login_step.user_launched_the_chrome_browser()"
 });
 formatter.result({
-  "duration": 10594993400,
+  "duration": 13639981400,
   "status": "passed"
 });
 formatter.match({
   "location": "login_step.user_opens_naukri_homepage()"
 });
 formatter.result({
-  "duration": 20875811600,
+  "duration": 25839924500,
   "status": "passed"
 });
 formatter.match({
   "location": "login_step.user_clicks_login()"
 });
 formatter.result({
-  "duration": 25900577500,
+  "duration": 29236889500,
   "status": "passed"
 });
 formatter.scenario({
@@ -105,36 +105,36 @@ formatter.match({
   "location": "advancesearch_step.user_launched_the_chromebrowser()"
 });
 formatter.result({
-  "duration": 9404129000,
+  "duration": 9523729400,
   "status": "passed"
 });
 formatter.match({
   "location": "advancesearch_step.user_opens_naukrihomepage()"
 });
 formatter.result({
-  "duration": 19395163200,
+  "duration": 24963880300,
   "status": "passed"
 });
 formatter.match({
   "location": "advancesearch_step.click_on_jobs_and_move_to_advancesearch()"
 });
 formatter.result({
-  "duration": 22831800400,
+  "duration": 16074192500,
   "status": "passed"
 });
 formatter.match({
   "location": "advancesearch_step.alert_page_is_opened_enter_all_the_required_fields()"
 });
 formatter.result({
-  "duration": 34427400900,
-  "error_message": "org.openqa.selenium.NoSuchElementException: no such element: Unable to locate element: {\"method\":\"xpath\",\"selector\":\"//*[@id\u003d\"dd_adv_workExp_year\"]/div[1]/span\"}\n  (Session info: chrome\u003d80.0.3987.149)\nFor documentation on this error, please visit: https://www.seleniumhq.org/exceptions/no_such_element.html\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:25:48\u0027\nSystem info: host: \u0027LAPTOP-0E4BENU1\u0027, ip: \u0027192.168.43.158\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u002712.0.2\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 80.0.3987.149, chrome: {chromedriverVersion: 80.0.3987.106 (f68069574609..., userDataDir: C:\\Users\\hp\\AppData\\Local\\T...}, goog:chromeOptions: {debuggerAddress: localhost:62610}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify}\nSession ID: f65dab2f6b9bd32efb4077d7affc6854\n*** Element info: {Using\u003dxpath, value\u003d//*[@id\u003d\"dd_adv_workExp_year\"]/div[1]/span}\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat java.base/jdk.internal.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat java.base/jdk.internal.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.base/java.lang.reflect.Constructor.newInstanceWithCaller(Constructor.java:500)\r\n\tat java.base/java.lang.reflect.Constructor.newInstance(Constructor.java:481)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:187)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:122)\r\n\tat org.openqa.selenium.remote.http.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:49)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat com.pages.Advancesearch_Main.enterdata(Advancesearch_Main.java:105)\r\n\tat com.stepdefinition.advancesearch_step.alert_page_is_opened_enter_all_the_required_fields(advancesearch_step.java:33)\r\n\tat ✽.When alert page is opened enter all the required fields(C:/Users/hp/Downloads/NaukariProject (2)/NaukariProject/src/main/resources/features/TestCase.feature:17)\r\n",
-  "status": "failed"
+  "duration": 6298199700,
+  "status": "passed"
 });
 formatter.match({
   "location": "advancesearch_step.click_on_advancesearch()"
 });
 formatter.result({
-  "status": "skipped"
+  "duration": 6765721300,
+  "status": "passed"
 });
 formatter.scenario({
   "line": 21,
@@ -169,21 +169,21 @@ formatter.match({
   "location": "about_step.user_has_launched_the_chromebrowser()"
 });
 formatter.result({
-  "duration": 9403768500,
+  "duration": 9890170500,
   "status": "passed"
 });
 formatter.match({
   "location": "about_step.user_has_opens_naukrihomepage()"
 });
 formatter.result({
-  "duration": 23555065000,
+  "duration": 19387989900,
   "status": "passed"
 });
 formatter.match({
   "location": "about_step.scroll_down_to_end_of_page_and_click_on_about_us()"
 });
 formatter.result({
-  "duration": 88029095600,
+  "duration": 37990981400,
   "status": "passed"
 });
 formatter.scenario({
@@ -224,28 +224,28 @@ formatter.match({
   "location": "windowhandling_step.user_has_launched_the_chrome_browser()"
 });
 formatter.result({
-  "duration": 9434068400,
+  "duration": 9765460100,
   "status": "passed"
 });
 formatter.match({
   "location": "windowhandling_step.user_has_opens_naukri_homepage()"
 });
 formatter.result({
-  "duration": 22976505300,
+  "duration": 14471071800,
   "status": "passed"
 });
 formatter.match({
   "location": "windowhandling_step.click_on_govtjobs_and_choose_defence_govt_jobs()"
 });
 formatter.result({
-  "duration": 19454561000,
+  "duration": 14962681800,
   "status": "passed"
 });
 formatter.match({
   "location": "windowhandling_step.click_on_one_of_option_based_on_requirement()"
 });
 formatter.result({
-  "duration": 4677835100,
+  "duration": 5024623400,
   "status": "passed"
 });
 formatter.scenario({
@@ -286,28 +286,28 @@ formatter.match({
   "location": "retrievecontact_step.user_has_launched_the_browser()"
 });
 formatter.result({
-  "duration": 10116803700,
+  "duration": 9818438600,
   "status": "passed"
 });
 formatter.match({
   "location": "retrievecontact_step.users_has_opens_naukri_homepage()"
 });
 formatter.result({
-  "duration": 20005208500,
+  "duration": 17279731700,
   "status": "passed"
 });
 formatter.match({
   "location": "retrievecontact_step.click_on_contact_us()"
 });
 formatter.result({
-  "duration": 17869883600,
+  "duration": 21698793700,
   "status": "passed"
 });
 formatter.match({
   "location": "retrievecontact_step.then_retrive_contact_information()"
 });
 formatter.result({
-  "duration": 5163658900,
+  "duration": 5429722100,
   "status": "passed"
 });
 });
--- NaukariProject/src/test/java/com/pages/Advancesearch_Main.java
+++ NaukariProject/src/test/java/com/pages/Advancesearch_Main.java
@@ -102,22 +102,16 @@ public class Advancesearch_Main {
 	public void  enterdata() throws InterruptedException {
 		driver.findElement(By.xpath("//*[@id=\"advKeyskills\"]/div[1]/div[2]/input")).sendKeys("Javadevloper");
 		driver.findElement(By.xpath("//*[@id=\"Sug_advLocation\"]")).sendKeys("Hydrabad");
-		driver.findElement(By.xpath("//*[@id=\"dd_adv_workExp_year\"]/div[1]/span")).click();
-		driver.findElement(By.xpath("//*[@id=\"dd_cja_expyr3\"]")).click();
-		driver.findElement(By.xpath("//*[@id=\"dd_adv_workExp_month\"]/div[1]/span")).click();
-		driver.findElement(By.xpath("//*[@id=\"dd_cja_expmn6\"]")).click();
-		driver.findElement(By.xpath("//*[@id=\"dd_adv_exp_min\"]/div[1]/span")).click();
-		driver.findElement(By.xpath("//*[@id=\"dd_cja_min7\"]")).click();
 		Actions act = new Actions(driver);
 		driver.findElement(By.xpath("//*[@id=\"ddAdvIndusrty\"]/div[1]/ul/li/div/input")).click();
 		WebElement c =driver.findElement(By.xpath("//*[@id=\"ul_ddAdvIndusrty\"]/div/div[1]/ul/li[22]/a"));
 		act.moveToElement(c).click().build().perform();
-		driver.findElement(By.xpath("/html/body")).click();
+		driver.findElement(By.xpath("//*[@id=\"advFrm\"]/div[2]/div[1]/label")).click();
 		driver.findElement(By.xpath("//*[@id=\"adv_jobCategory\"]")).click();
-		WebElement d =driver.findElement(By.xpath("//*[@id=\"24\"]"));
+		WebElement d =driver.findElement(By.xpath("//*[@id=\"8\"]"));
 		act.moveToElement(d).click().build().perform();
-		driver.findElement(By.xpath("//*[@id=\"advFrm\"]/div[7]/div[2]/div/div[2]/a")).click();
-		driver.findElement(By.xpath("//*[@id=\"advFrm\"]/div[8]/div[2]/div/div[2]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"advFrm\"]/div[5]/div[2]/div/div[2]/a")).click();
+		driver.findElement(By.xpath("//*[@id=\"advFrm\"]/div[6]/div[2]/div/div[2]/a")).click();
 	}
 	//Clicking on advance search button
 	public void advancesearch() {
